40cf2ce93eceb693cddf80d9ede2db5c67b84612
James Palmer <jms.plmr@gmail.com>
2017-02-22 14:36:34 -0700

    Test of scripture container class

---
+++ app/src/test/java/com/byui_cs/jjmn/ponderize/ScriptureContainerTest.java
@@ -0,0 +1,23 @@
+package com.byui_cs.jjmn.ponderize;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+/**
+ * Created by James Palmer on 22-Feb-17.
+ */
+
+public class ScriptureContainerTest {
+
+  ScriptureContainer sc = new ScriptureContainer("Ether",12,4);
+
+  @Test
+  public void scriptureContainerShouldDisplayScripture()throws Exception{
+
+    assertEquals("Ether 12:4", sc.toString());
+    assertNotEquals("Ether 12-4",sc.toString());
+  }
+
+}
