c80f8d0ff6160df417c7e5fb41529541129eb1f0
Michael <mgraver@users.noreply.github.com>
2017-02-23 16:20:53 -0700

    added scripture loader function

---
+++ app/src/main/java/edu/byui_cs/jjmn/ponderize/scriptureLoader.java
@@ -0,0 +1,14 @@
+package edu.byui_cs.jjmn.ponderize;
+
+import java.io.File;
+
+/**
+ * Created by Michael on 2/23/17.
+ */
+
+public class scriptureLoader {
+
+    public ScriptureContainer loadScripture(File aFile) {
+
+    }
+}
