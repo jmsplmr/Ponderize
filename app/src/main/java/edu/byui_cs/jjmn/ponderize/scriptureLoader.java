a4149390994ea7c6b725073d4a6d28187eacefbe
Michael <mgraver@users.noreply.github.com>
2017-02-23 16:43:51 -0700

    scripture loader finished

--- app/src/main/java/edu/byui_cs/jjmn/ponderize/scriptureLoader.java
+++ app/src/main/java/edu/byui_cs/jjmn/ponderize/scriptureLoader.java
@@ -1,6 +1,11 @@
 package edu.byui_cs.jjmn.ponderize;
 
+import android.graphics.Path;
+
+import com.google.gson.Gson;
+
 import java.io.File;
+import java.io.FileInputStream;
 
 /**
  * Created by Michael on 2/23/17.
@@ -9,6 +14,28 @@ import java.io.File;
 public class scriptureLoader {
 
     public ScriptureContainer loadScripture(File aFile) {
+
+        ScriptureContainer scripture = new ScriptureContainer();
+        try {
+            //Reading all the bytes into an array.
+            FileInputStream fileInput = new FileInputStream(aFile);
+            byte [] fileContents = new byte[(int) aFile.length()];
+            fileInput.read(fileContents);
+            fileInput.close();
+
+            //Turning bytes into a string object.
+            String scriptureJson = new String(fileContents, "UTF-8");
+
+            //Turning back into a scripture.
+            Gson gsonConverter = new Gson();
+            scripture = gsonConverter.fromJson(scriptureJson, ScriptureContainer.class);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return scripture;
 
     }
 }
