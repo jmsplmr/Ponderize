6a182115f8d25b91782e1743dc4ac5554b157e0a
Michael <mgraver@users.noreply.github.com>
2017-02-24 13:30:51 -0700

    Reverted cahnges after merging. Pay more attention before merge.

--- app/src/main/java/edu/byui_cs/jjmn/ponderize/ScriptureViewActivity.java
+++ app/src/main/java/edu/byui_cs/jjmn/ponderize/ScriptureViewActivity.java
@@ -3,8 +3,11 @@ package edu.byui_cs.jjmn.ponderize;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.byui_cs.jjmn.ponderize.R;
+
+import java.io.File;
 
 public class ScriptureViewActivity extends AppActivity {
 
@@ -24,8 +27,18 @@ public class ScriptureViewActivity extends AppActivity {
     return super.onOptionsItemSelected(item);
   }
 
-  public void testScriptureStorage() {
+  public void testStorage(View view) {
+      File aFile = new File(this.getFilesDir(),"json.txt");
+      ScriptureContainer scripture = new ScriptureContainer("Ether", 5, 5);
 
+      new scriptureSaver().saveScripture(scripture, aFile);
+      System.out.println("Saved file");
+
+      ScriptureContainer loadScripture;
+      loadScripture = new scriptureLoader().loadScripture(aFile);
+
+      System.out.println("Loaded: ");
+      System.out.println(loadScripture.getBook() + " " + loadScripture.getChapter() + " " + loadScripture.getVerse());
   }
 
 
