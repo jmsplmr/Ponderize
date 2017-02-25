b3497676635ada694f5f39d28d80b9b43ef49e67
Michael <mgraver@users.noreply.github.com>
2017-02-25 14:43:18 -0700

    Start a new class call scripture storage that will handle the loading and the saving of all scriptures.

---
+++ app/src/main/java/edu/byui_cs/jjmn/ponderize/ScriptureStorage.java
@@ -0,0 +1,12 @@
+package edu.byui_cs.jjmn.ponderize;
+
+/**
+ * Created by Michael on 2/25/17.
+ */
+
+public class ScriptureStorage {
+
+    //TODO: add scripture storage and saver.
+    //TODO: add a load all scriptures function.
+    //TODO: add a funtion that get file directory and turns scriptures names into files to read the files in.
+}
