46392917751ebcebf2f21e99fd9bab7c471d888e
Joseph <joey@koetting.org>
2017-02-25 14:16:12 -0700

    Added Scripture Adapter and List row XML file.
    Test code in main

--- app/src/main/java/edu/byui_cs/jjmn/ponderize/MainActivity.java
+++ app/src/main/java/edu/byui_cs/jjmn/ponderize/MainActivity.java
@@ -20,40 +20,47 @@ import static com.byui_cs.jjmn.ponderize.R.layout.activity_main;
 
 public class MainActivity extends AppCompatActivity {
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    Log.v(getClass().getSimpleName(), "Create main activity.");
-    super.onCreate(savedInstanceState);
-    setContentView(activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.v(getClass().getSimpleName(), "Create main activity.");
+        super.onCreate(savedInstanceState);
+        setContentView(activity_main);
 
-      //###########################################
-      //******* JOE TEST CODE DO NOT DELETE *******
-      //###########################################
-    ArrayList<ScriptureContainer> sc = new ArrayList<>();
+        //###########################################
+        //******* JOE TEST CODE DO NOT DELETE *******
+        //###########################################
 
-    ScriptureContainer a = new ScriptureContainer("Mark", 4, 5);
-    ScriptureContainer b = new ScriptureContainer("James", 5, 3);
-    ScriptureContainer c = new ScriptureContainer("Hockey", 6, 7);
-    ScriptureContainer d = new ScriptureContainer("Falron", 7, 3);
+        // init array
+        ArrayList<ScriptureContainer> sc = new ArrayList<>();
 
-    sc.add(0, a);
-    sc.add(1, b);
-    sc.add(2, c);
-    sc.add(3, d);
+        // init scriptures
+        ScriptureContainer a = new ScriptureContainer("Mark", 4, 5);
+        ScriptureContainer b = new ScriptureContainer("James", 5, 3);
+        ScriptureContainer c = new ScriptureContainer("Hockey", 6, 7);
+        ScriptureContainer d = new ScriptureContainer("Falron", 7, 3);
 
-    ListView listView = (ListView) findViewById(R.id.listView);
+        // add scriptures to array
+        sc.add(0, a);
+        sc.add(1, b);
+        sc.add(2, c);
+        sc.add(3, d);
 
-    listView.getAdapter();
-   ScriptureAdapter itemsAdapter = new ScriptureAdapter(this, sc);
-    listView.setAdapter(itemsAdapter);
+        // grab list view reference
+        ListView listView = (ListView) findViewById(R.id.listView);
 
-      //###########################################
-      //******* JOE TEST CODE DO NOT DELETE *******
-      //###########################################
-  }
+        // create new scripture adapter
+        ScriptureAdapter itemsAdapter = new ScriptureAdapter(this, sc);
+
+        // set list views adapter to new scripture adapter
+        listView.setAdapter(itemsAdapter);
+
+        //###########################################
+        //******* JOE TEST CODE DO NOT DELETE *******
+        //###########################################
+    }
 
     public void goToView(View view) {
-      Intent intent = new Intent(this, ScriptureViewActivity.class);
-      startActivity(intent);
+        Intent intent = new Intent(this, ScriptureViewActivity.class);
+        startActivity(intent);
     }
 }
