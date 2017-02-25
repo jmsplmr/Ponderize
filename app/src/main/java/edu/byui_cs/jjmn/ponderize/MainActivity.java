f3d8162ef16563860885b2966687169fdffa7753
Nick <nicolasw.palmer@gmail.com>
2017-02-25 13:45:10 -0700

    Made the UI

--- app/src/main/java/edu/byui_cs/jjmn/ponderize/MainActivity.java
+++ app/src/main/java/edu/byui_cs/jjmn/ponderize/MainActivity.java
@@ -10,21 +10,54 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.TabHost;
 
+import edu.byui_cs.jjmn.ponderize.MemorizeQuizActivity;
 import edu.byui_cs.jjmn.ponderize.ScriptureViewActivity;
 
 import static com.byui_cs.jjmn.ponderize.R.layout.activity_main;
 
 public class MainActivity extends AppCompatActivity {
+
+  TabHost tabHost;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     Log.v(getClass().getSimpleName(), "Create main activity.");
     super.onCreate(savedInstanceState);
     setContentView(activity_main);
+
+    TabHost host = (TabHost)findViewById(R.id.tabHostMain);
+    host.setup();
+
+    //Progressing Tab
+    TabHost.TabSpec spec = host.newTabSpec("Progressing");
+    spec.setContent(R.id.Progressing);
+    spec.setIndicator("Progressing");
+    host.addTab(spec);
+
+    //Memorized Tab
+    spec = host.newTabSpec("Memorized");
+    spec.setContent(R.id.Memorized);
+    spec.setIndicator("Memorized");
+    host.addTab(spec);
   }
 
     public void goToView(View view) {
       Intent intent = new Intent(this, ScriptureViewActivity.class);
       startActivity(intent);
     }
+
+  //For navigation testing buttons
+  public void onScriptureBtnClick(View v) {
+    Intent i = new Intent(this, ScriptureViewActivity.class);
+    startActivity(i);
+  }
+
+  public void onQuizBtnClick(View v) {
+    Intent i = new Intent(this, MemorizeQuizActivity.class);
+    startActivity(i);
+  }
+
+
 }
--- app/src/main/java/edu/byui_cs/jjmn/ponderize/ScriptureViewActivity.java
+++ app/src/main/java/edu/byui_cs/jjmn/ponderize/ScriptureViewActivity.java
@@ -3,15 +3,33 @@ package edu.byui_cs.jjmn.ponderize;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TabHost;
 
 import com.byui_cs.jjmn.ponderize.R;
 
 public class ScriptureViewActivity extends AppActivity {
 
+  TabHost tabHost;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_scripture_view);
+
+    TabHost host = (TabHost)findViewById(R.id.tabHostScript);
+    host.setup();
+
+    //Progressing Tab
+    TabHost.TabSpec spec = host.newTabSpec("Scripture");
+    spec.setContent(R.id.Scripture);
+    spec.setIndicator("Scripture");
+    host.addTab(spec);
+
+    //Memorized Tab
+    spec = host.newTabSpec("Notes");
+    spec.setContent(R.id.Notes);
+    spec.setIndicator("Notes");
+    host.addTab(spec);
   }
 
   @Override
