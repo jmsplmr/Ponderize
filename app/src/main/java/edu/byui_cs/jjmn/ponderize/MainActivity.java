b3305f87616151ae45241aa59d3607fd05acb6ee
Michael <mgraver@users.noreply.github.com>
2017-02-26 22:20:05 -0700

    Added test code to save scriptures to a array list then save it to a file then reload back into a array. Success.  Put idea into Scripture storage class.

--- app/src/main/java/edu/byui_cs/jjmn/ponderize/MainActivity.java
+++ app/src/main/java/edu/byui_cs/jjmn/ponderize/MainActivity.java
@@ -11,7 +11,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.ListView;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.List;
+
 import edu.byui_cs.jjmn.ponderize.ScriptureAdapter;
 import edu.byui_cs.jjmn.ponderize.ScriptureContainer;
 import edu.byui_cs.jjmn.ponderize.ScriptureViewActivity;
@@ -31,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
       //###########################################
       //******* JOE TEST CODE DO NOT DELETE *******
       //###########################################
-    ArrayList<ScriptureContainer> sc = new ArrayList<>();
+    /*ArrayList<ScriptureContainer> sc = new ArrayList<>();
 
     ScriptureContainer a = new ScriptureContainer("Mark", 4, 5);
     ScriptureContainer b = new ScriptureContainer("James", 5, 3);
@@ -47,15 +60,58 @@ public class MainActivity extends AppCompatActivity {
 
     listView.getAdapter();
    ScriptureAdapter itemsAdapter = new ScriptureAdapter(this, sc);
-    listView.setAdapter(itemsAdapter);
+    listView.setAdapter(itemsAdapter);*/
 
       //###########################################
       //******* JOE TEST CODE DO NOT DELETE *******
       //###########################################
   }
 
-    public void goToView(View view) {
-      Intent intent = new Intent(this, ScriptureViewActivity.class);
-      startActivity(intent);
+    //TODO: Remove test code.
+    public void testCode(View view) {
+        List<ScriptureContainer> scriptureList = new ArrayList<ScriptureContainer>();
+
+        ScriptureContainer s1 = new ScriptureContainer("1 Nephi", 1, 1);
+        ScriptureContainer s2 = new ScriptureContainer("Moroni", 1, 15);
+        ScriptureContainer s3 = new ScriptureContainer("Alma", 37, 6);
+        ScriptureContainer s4 = new ScriptureContainer("Ether", 7, 7);
+
+        scriptureList.add(s1);
+        scriptureList.add(s2);
+        scriptureList.add(s3);
+        scriptureList.add(s4);
+
+        Gson gson = new Gson();
+        String jsonList = gson.toJson(scriptureList);
+
+        File aFile = new File(this.getFilesDir(), "test.txt");
+        FileWriter writer = null;
+
+        try {
+            writer = new FileWriter(aFile);
+            writer.write(jsonList);
+            System.out.println("Wrote to file.");
+            writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        File loadFile = new File(this.getFilesDir(), "test.txt");
+
+        try {
+            FileInputStream input = new FileInputStream(loadFile);
+            byte [] fileContent = new byte[(int)loadFile.length()];
+            input.read(fileContent);
+            System.out.println("Loaded File");
+
+            String json = new String(fileContent);
+            Type listType = new TypeToken<ArrayList<ScriptureContainer>>(){}.getType();
+            ArrayList<ScriptureContainer> loadedList = gson.fromJson(json, listType);
+            System.out.println(loadedList.size());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
