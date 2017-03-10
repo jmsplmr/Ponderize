de6690ae18663f11bb228a381d6ba96ad511451a
Michael <mgraver@users.noreply.github.com>
2017-03-10 14:09:45 -0700

    added my own side bar listener

--- app/src/main/java/edu/byui_cs/jjmn/ponderize/PracticeActivity.java
+++ app/src/main/java/edu/byui_cs/jjmn/ponderize/PracticeActivity.java
@@ -1,8 +1,11 @@
 package edu.byui_cs.jjmn.ponderize;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.byui_cs.jjmn.ponderize.R;
 
@@ -20,5 +23,46 @@ public class PracticeActivity extends AppCompatActivity {
 
         TextView aView = (TextView) findViewById(R.id.practiceView);
         aView.setText(verse);
+
+        SeekBar seekBar = (SeekBar) findViewById(R.id.seekBar);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                CharSequence text = "Progress: " + progress;
+
+                Toast toast = Toast.makeText(getApplicationContext(), text, 1);
+                toast.show();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                //Auto generated, don't need.
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                //Auto generated, don't need.
+            }
+        });
+    }
+
+    /**************************************
+     * removeWords
+     * Removes word from the verse to
+     * practice memorizing.
+     *************************************/
+
+    public void removeWords() {
+
+    }
+
+    /**********************************
+     * addWords
+     * add words back to the string to
+     * check your self.
+     *********************************/
+    public void addWords() {
+
     }
 }
