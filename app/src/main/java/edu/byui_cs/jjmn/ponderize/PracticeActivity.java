fea65cb2c5c52713a03490ae1d3491b8b66b4fab
Michael <mgraver@users.noreply.github.com>
2017-03-10 13:35:15 -0700

    change the idea for a text view and on activity start to change view text for a preset string. Needs to be changed for later.

--- app/src/main/java/edu/byui_cs/jjmn/ponderize/PracticeActivity.java
+++ app/src/main/java/edu/byui_cs/jjmn/ponderize/PracticeActivity.java
@@ -2,14 +2,23 @@ package edu.byui_cs.jjmn.ponderize;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.byui_cs.jjmn.ponderize.R;
 
+import java.util.Stack;
+
 public class PracticeActivity extends AppCompatActivity {
+
+    private Stack indexRemove = new Stack();
+    private String verse = "and it came to pass that the lord flooded the earth.";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practice);
+
+        TextView aView = (TextView) findViewById(R.id.practiceView);
+        aView.setText(verse);
     }
 }
