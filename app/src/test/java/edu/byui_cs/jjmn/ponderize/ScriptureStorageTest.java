a0c5fd7672b2fd772d38a7d847a4ae98169141d9
James Palmer <pal15023@byui.edu>
2017-03-17 17:10:09 -0600

    Add mocking to unit test

--- app/src/test/java/edu/byui_cs/jjmn/ponderize/ScriptureStorageTest.java
+++ app/src/test/java/edu/byui_cs/jjmn/ponderize/ScriptureStorageTest.java
@@ -1,44 +1,52 @@
 package edu.byui_cs.jjmn.ponderize;
 
+import android.content.Context;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnit;
 
 import java.io.File;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
+import static org.mockito.Mockito.mock;
 
 /**
- * Created by jmspl on 17-Mar-17.
+ * Created by James Palmer on 17-Mar-17.
  */
 
 public class ScriptureStorageTest {
   
-  public ScriptureContainer scripture;
-  public final String BOOK = "Ether";
-  public final int CHAPTER = 12;
-  public final int VERSE = 4;
-  public final String TEXT = "Wherefore, whoso believeth in God might with surety hope for a " +
+  private ScriptureContainer scripture;
+  private final String BOOK = "Ether";
+  private final int CHAPTER = 12;
+  private final int VERSE = 4;
+  private final String TEXT = "Wherefore, whoso believeth in God might with surety hope for a " +
                                    "better world, yea, even a place at the right hand of God, " +
                                    "which hope cometh of faith, maketh an anchor to the souls of" +
                                    " men, which would make them sure and steadfast, always " +
                                    "abounding in good works, being led to glorify God.";
+  @Mock
+  private Context context;
   
   @Before
-  public void setUp() throws Exception {
-    scripture = new ScriptureContainer (BOOK,CHAPTER,VERSE);
+  public void setUp () throws Exception {
+    scripture = new ScriptureContainer (BOOK, CHAPTER, VERSE);
     scripture.setText (TEXT);
+    context = mock(MainActivity.class);
   }
   
   @Test
   public void scriptureStorage_Should_SaveScripture () throws Exception {
     ScriptureStorage scriptureSave = new ScriptureStorage ();
-  
-    File file = new File ("","Ether124.txt");
-    scriptureSave.saveScripture (scripture,file);
+    
+    File file = new File (context.getFilesDir(), "Ether124.txt");
+    scriptureSave.saveScripture (scripture, file);
     
     ScriptureContainer loadedScripture = scriptureSave.loadScripture (file);
     
-    assertEquals (scripture.getReference (),loadedScripture.getReference () );
+    assertEquals (scripture.getReference (), loadedScripture.getReference ());
   }
 }
