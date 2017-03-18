e3e7921a81866c63c03cf1f75e4c7d34d379bef8
James Palmer <pal15023@byui.edu>
2017-03-18 10:27:24 -0600

    Reformat

--- app/src/main/java/edu/byui_cs/jjmn/ponderize/Notification_receiver.java
+++ app/src/main/java/edu/byui_cs/jjmn/ponderize/Notification_receiver.java
@@ -12,32 +12,32 @@ import android.support.v4.app.NotificationCompat;
  */
 
 public class Notification_receiver extends BroadcastReceiver {
-
-    @Override
-    public void onReceive(Context context, Intent intent) {
-
-        // no idea
-        NotificationManager notificationManager = (NotificationManager)
-                context.getSystemService((context.NOTIFICATION_SERVICE));
-
-        // When notification is clicked, open Main Activity
-        Intent repeating_intent = new Intent(context, MainActivity.class);
-        repeating_intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-
-        // no idea
-        PendingIntent pendingIntent = PendingIntent.getActivity(context,
-                100, repeating_intent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-
-        // builds the notification
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
-                .setContentIntent(pendingIntent)
-                .setSmallIcon(android.R.drawable.arrow_up_float)
-                .setContentTitle("Ponderize")
-                .setContentText("Time to Ponder some Scriptures!")
-                .setAutoCancel(true);
-
-        // runs notification
-        notificationManager.notify(100, builder.build());
-    }
+  
+  @Override
+  public void onReceive (Context context, Intent intent) {
+    
+    // no idea
+    NotificationManager notificationManager = (NotificationManager)
+                                                    context.getSystemService ((context.NOTIFICATION_SERVICE));
+    
+    // When notification is clicked, open Main Activity
+    Intent repeating_intent = new Intent (context, MainActivity.class);
+    repeating_intent.setFlags (Intent.FLAG_ACTIVITY_CLEAR_TOP);
+    
+    // no idea
+    PendingIntent pendingIntent = PendingIntent.getActivity (context,
+          100, repeating_intent, PendingIntent.FLAG_UPDATE_CURRENT);
+    
+    
+    // builds the notification
+    NotificationCompat.Builder builder = new NotificationCompat.Builder (context)
+                                               .setContentIntent (pendingIntent)
+                                               .setSmallIcon (android.R.drawable.arrow_up_float)
+                                               .setContentTitle ("Ponderize")
+                                               .setContentText ("Time to Ponder some Scriptures!")
+                                               .setAutoCancel (true);
+    
+    // runs notification
+    notificationManager.notify (100, builder.build ());
+  }
 }
